d20065e23b187a00e0b9c8726eee2e6335d96953
Lihao Deng <lihao-deng@outlook.com>
2022-06-21 23:11:20 +0800

    1

--- xiaoqiandao.py
+++ xiaoqiandao.py
@@ -16,10 +16,8 @@ access_token = input("请输入token:")
 # 获取打卡列表
 def getlist():
     url = f"https://api-xcx-qunsou.weiyoubot.cn/xcx/checkin/v3/list?type=5&page=1&count=10&access_token={access_token}&tag=0&keyword="
-
     page = requests.get(url, headers=headers)
-
-    dict_page = json.loads(page.content)
+    dict_page = page.json()
 
     try:
         datas = dict_page['data']
@@ -34,15 +32,11 @@ def getlist():
 
     for data in datas:
         title = data['title']
-
         cid = data['cid']
-
         owner = data['owner']
 
         checkin = {"id": id, "title": title, "owner": owner, "cid": cid}
-
         checkins.append(checkin)
-
         print(checkin)
 
         id += 1
@@ -50,22 +44,17 @@ def getlist():
     id = input("请输入对应的id:")
 
     cid = checkins[int(id)]["cid"]
-
     return cid
 
 
 # 获取打卡指定的位置信息
 def getcid(cid):
     url = f"https://api-xcx-qunsou.weiyoubot.cn/xcx/checkin/v4/detail?cid={cid}&access_token={access_token}&tag=0&random_num=0.9361229083976608"
-
     page = requests.get(url, headers=headers)
-
-    dict_page = json.loads(page.content)
+    dict_page = page.json()
 
     val = dict_page['data']['locations'][0]['address']
-
     lat = round(float(dict_page['data']['locations'][0]['latitude']), 5)
-
     lon = round(float(dict_page['data']['locations'][0]['longitude']), 5)
 
     print('\n', val, "\n", lat, '\n', lon)
@@ -75,7 +64,6 @@ def getcid(cid):
 
 def qiandao(cid, val, lat, lon):
     url = "https://api-xcx-qunsou.weiyoubot.cn/xcx/checkin/v3/doit"
-
     data = {
         "cid": cid,
         "access_token": access_token,
@@ -89,7 +77,6 @@ def qiandao(cid, val, lat, lon):
 
     # data表单必须先处理成json数据后才能发送
     page = requests.post(url, headers=headers, data=json.dumps(data))
-
     print("\n" + page.text)
 
 
